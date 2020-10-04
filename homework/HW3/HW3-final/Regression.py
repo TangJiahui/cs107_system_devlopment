c091feb0d4cbc283f6ef8c7f5bf28673f6aaf760
Tang Jiahui <TangJiahui@users.noreply.github.com>
2020-10-04 15:30:09 +0800

    update Regresion class and model scoring

--- homework/HW3/HW3-final/Regression.py
+++ homework/HW3/HW3-final/Regression.py
@@ -14,11 +14,11 @@ class Regression():
         raise NotImplementedError
 
     def predict(self, X):
-        return np.dot(self.param["coefficient"], X) + self.param["intercept"]
+        return np.dot(X, self.param["coefficient"]) + self.param["intercept"]
 
     def score(self, X, y):
-        SSE = (y - predict(X))**2
-        SST = (y - np.mean(y))**2
+        SSE = np.sum((y - self.predict(X))**2)
+        SST = np.sum((y - np.mean(y))**2)
         return 1-SSE/SST
 
 
@@ -27,8 +27,8 @@ class LinearRegression(Regression):
         new_col = np.array([[1]* X.shape[0]]).T
         new_X =  np.append(X, new_col, axis=1)
         best_fit = np.dot(np.dot(np.linalg.pinv(np.dot(new_X.T,new_X)),new_X.T),y)
-        self.param["coefficient"] = best_fit[:X.shape[0],]
-        self.param["intercept"] = best_fit[X.shape[0]:,]
+        self.param["coefficient"] = best_fit[:X.shape[1]]
+        self.param["intercept"] = best_fit[X.shape[1]:]
         
 
 
@@ -39,10 +39,7 @@ class RidgeRegression(LinearRegression):
     def fit(self, X, y):
         new_col = np.array([[1]* X.shape[0]]).T
         new_X =  np.append(X, new_col, axis=1)
-        print(new_X)
         tao = self.alpha * np.identity(new_X.shape[1])
-        print(tao)
-        print(np.dot(tao.T, tao))
         best_fit = np.dot(np.dot(np.linalg.pinv(np.dot(new_X.T, new_X)+np.dot(tao.T, tao)), new_X.T),y)
-        self.param["coefficient"] = best_fit[:X.shape[0],]
-        self.param["intercept"] = best_fit[X.shape[0]:,]
+        self.param["coefficient"] = best_fit[:X.shape[1],]
+        self.param["intercept"] = best_fit[X.shape[1]:,]
---
+++ homework/HW3/HW3-final/model_scoring.py
@@ -0,0 +1,30 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+#import regression classes
+from Regression import Regression
+from Regression import LinearRegression
+from Regression import RidgeRegression
+
+dataset = datasets.load_boston()
+X_train, X_test, y_train, y_test = train_test_split(dataset['data'], 
+                                                    dataset['target'], 
+                                                    test_size=0.2, 
+                                                    random_state=42)
+
+alpha = 0.1
+linreg = LinearRegression()
+ridreg = RidgeRegression()
+ridreg.set_params(alpha=alpha)
+models = [linreg, ridreg]
+
+model_scores = []
+for model in models:
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    model_scores.append(score)
+    print(str(type(model).__name__)+"has R^2 score of: " + str(score))
+
+best_model = models[model_scores.index(max(model_scores))]
+print("The best model is " + str(type(best_model).__name__))
+print("And params for the best model are: ")
+print(best_model.get_params())
