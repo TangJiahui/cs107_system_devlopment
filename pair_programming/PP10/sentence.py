aac3a5a6ac1a8086b47566566b7ab642f986fee3
Tang Jiahui <tangjiahui0610@gmail.com>
2020-11-09 20:53:03 +0800

    update test case

--- pair_programming/PP10/sentence.py
+++ pair_programming/PP10/sentence.py
@@ -7,26 +7,26 @@ class Sentence: # An iterable
         self.text = text
         self.words = text.split()
 
-
     def __iter__(self):
         for w in self.words:
             yield w
         
     def __repr__(self):
         return 'Sentence(%s)' % reprlib.repr(self.text)
-        
+
+# demo
 text = "hello this is a test case"
 s = Sentence(text)
 
 g = s.__iter__()
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
-
-g = s.__iter__()
 for vals in g:
     print(vals)
+
+g = s.__iter__()
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
